f31ce769a9a8cfcafb947da59225d249457eded2
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2024-04-26 00:17:37 -0500

    smooth movement. broken rotation

--- Player.cs
+++ Player.cs
@@ -1,54 +1,103 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : Node3D
 {
-	enum PlayerState {STANDING, MOVING}
+	private const float SPEED = 2.0f;
 
-	private Vector2I gridPosition;
+	enum PlayerState {IDLE, MOVING, TURNING}
+	private PlayerState state = PlayerState.IDLE;
+	private Vector2 gridPosition;
+	private Vector3 oldWorldPosition;
+	private Vector3 newWorldPosition;
+	private Vector2 newGridPosition;
 
-	// Called when the node enters the scene tree for the first time.
+	private Vector3 oldAngle;
+	private Vector3 newAngle;
+	private float moveProgress = 0.0f;
+
 	public override void _Ready()
 	{
-		gridPosition = new Vector2I(1,1);
-		SetNewPosition(gridPosition);
+		oldAngle = RotationDegrees;
+		gridPosition = new Vector2(1,1);
+		//SetNewPosition(gridPosition);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		switch(state)
+		{
+			case PlayerState.MOVING:
+				moveProgress = Math.Clamp(moveProgress + (float)delta * SPEED, 0.0f, 1.0f);
+				GlobalPosition = oldWorldPosition.Lerp(newWorldPosition, moveProgress);
+				if (moveProgress == 1.0f)
+				{
+					moveProgress = 0.0f;
+					oldWorldPosition = GlobalPosition;
+					gridPosition = newGridPosition;
+					state = PlayerState.IDLE;
+				}
+				break;
+			case PlayerState.TURNING:
+				moveProgress = Math.Clamp(moveProgress + (float)delta * SPEED * 2.0f, 0.0f, 1.0f);
+				GlobalRotationDegrees = oldAngle.Lerp(newAngle, moveProgress);
+				if (moveProgress == 1.0f)
+				{
+					moveProgress = 0.0f;
+					oldAngle = newAngle.Round();
+					state = PlayerState.IDLE;
+				}
+				break;
+			case PlayerState.IDLE:
+				if (Input.IsActionPressed("forward"))
+				{
+					var directionOffset = new Vector2(0,-1);
+					SetNewPosition(directionOffset.Rotated(Rotation.Y));
+				}
+				if (Input.IsActionPressed("back"))
+				{
+					var directionOffset = new Vector2(0,1);
+					SetNewPosition(directionOffset.Rotated(Rotation.Y));
+				}
+				if (Input.IsActionPressed("left"))
+				{
+					var directionOffset = new Vector2(-1,0);
+					SetNewPosition(directionOffset.Rotated(Rotation.Y));
+				}
+				if (Input.IsActionPressed("right"))
+				{
+					var directionOffset = new Vector2(1,0);
+					SetNewPosition(directionOffset.Rotated(Rotation.Y));
+				}
+				if (Input.IsActionJustPressed("turn_left"))
+				{
+					SetNewRotation(90);
+				}
+				if (Input.IsActionJustPressed("turn_right"))
+				{
+					SetNewRotation(-90);
+				}
+				break;
+		}
 	}
 
     public override void _Input(InputEvent @event)
     {
-		if(@event.IsActionPressed("forward"))
-		{
-			gridPosition += new Vector2I(0,-1);
-			SetNewPosition(gridPosition);
-		}
-		if(@event.IsActionPressed("back"))
-		{
-			gridPosition += new Vector2I(0,1);
-			SetNewPosition(gridPosition);
-		}
-		if(@event.IsActionPressed("left"))
-		{
-			gridPosition += new Vector2I(-1,0);
-			SetNewPosition(gridPosition);
-		}
-		if(@event.IsActionPressed("right"))
-		{
-			gridPosition += new Vector2I(1,0);
-			SetNewPosition(gridPosition);
-		}
 
-		GD.Print(gridPosition);	
-        //base._Input(@event);
     }
 
-	public void SetNewPosition(Vector2I newGridPos)
+	private void SetNewPosition(Vector2 directionOffset)
 	{
-		var translatedPos = newGridPos * 2;
-		GlobalPosition = new Vector3(translatedPos.X + 1, 0.5f, translatedPos.Y + 1);
+		state = PlayerState.MOVING;
+		newGridPosition = gridPosition + directionOffset;
+		var translatedPos = newGridPosition * 2;
+		newWorldPosition = new Vector3(translatedPos.X + 1, 0.5f, translatedPos.Y + 1);
+	}
+
+	private void SetNewRotation(float angleOffset)
+	{
+		state = PlayerState.TURNING;
+		newAngle = oldAngle + new Vector3(0, angleOffset, 0);
 	}
 }
