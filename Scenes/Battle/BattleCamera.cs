d4c24f18a11b4c5b88c1a658df3efcb0cc93ae0e
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2024-05-26 16:57:59 -0500

    UI Tracking

--- Scenes/Battle/BattleCamera.cs
+++ Scenes/Battle/BattleCamera.cs
@@ -1,13 +1,19 @@
 using Godot;
+using Scenes.Enemy;
 using System;
 
 public partial class BattleCamera : Camera3D
 {
 	double progress = 0;
 
+	private HealthBar healthBar;
+	private Enemy enemy;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		healthBar = GetNode<HealthBar>("ProgressBar");
+		enemy = GetParent().GetNode<Enemy>("Marker3D/Enemy");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +22,8 @@ public partial class BattleCamera : Camera3D
 		progress += delta;
 		//GlobalPosition = new Vector3((float)Math.Sin(progress) * 3, 0, (float)Math.Cos(progress) * 3);
 		GlobalPosition = new Vector3((float)Math.Sin(progress) * 2 + 1, 0, (float)Math.Sin(progress)+2);
-		GD.Print(GlobalPosition);
+
+		healthBar.Position = UnprojectPosition(enemy.GlobalPosition);
 		//LookAt(Vector3.Zero);
 	}
 }
